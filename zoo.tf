3f90fe90b60e5375a9575488e6af9eb60d378981
mkol5222 <mkoldov@gmail.com>
2024-01-08 10:25:57 +0000

    map

--- zoo.tf
+++ zoo.tf
@@ -24,6 +24,8 @@ locals {
     animalNames = [for animal in local.zooJson.zoo : animal.name]
 
     animals = [for animal in local.zooJson.zoo : animal]
+
+    animalsMap =  { for animal in local.zooJson.zoo : animal.name => animal }
 }
 
 output "animalNames" {
@@ -33,11 +35,15 @@ output "animalNames" {
 output "animals" {
     value = local.animals
 }
-# resource "random_pet" "zoo_animals" {
 
-#   for_each = local.animals
-#   keepers = {
-#     name = each.value.name
-#   }
-#   length = each.value.len
-# }
+output "animalsMap" {
+    value = local.animalsMap
+}
+resource "random_pet" "zoo_animals" {
+
+  for_each = local.animalsMap
+  keepers = {
+    name = each.value.name
+  }
+  length = each.value.len
+}
